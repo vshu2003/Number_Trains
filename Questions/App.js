bc490ef9edb73f941d1d73c7a1312de1bb55e646
vshu2003 <101983732+vshu2003@users.noreply.github.com>
2023-08-22 20:28:37 +0530

    Add files via upload

---
+++ Questions/App.js
@@ -0,0 +1,40 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          s
+        >
+          Learn React
+        </a>
+        <center>
+          <a href="http://20.244.56.144/numbers/primes" target="_blank">
+            Prime
+          </a>
+          <a href="http://20.244.56.144/numbers/fibo" target="_blank">
+            Fibo
+          </a>
+          <a href="http://20.244.56.144/numbers/odd" target="_blank">
+            Odd
+          </a>
+          <a href="http://20.244.56.144/numbers/rand" target="_blank">
+            Rand
+          </a>
+        </center>
+      </header>
+    </div>
+  );
+}
+
+export default App;
